6788a97e9f0caf04c2b7650ef516dd5d9ca9cef4
Programador <Programador@CATUMBELA>
2020-12-07 01:45:27 +0100

    Atualizando o código

--- SistemaUsuarios/src/main/java/ao/com/catumbela/sistemausuarios/bean/UsuarioBean.java
+++ SistemaUsuarios/src/main/java/ao/com/catumbela/sistemausuarios/bean/UsuarioBean.java
@@ -25,44 +25,70 @@ public class UsuarioBean implements Serializable {
 	private CargoDAO cargoDAO;
 	private List<Cargo> cargos;
 	private UsuarioDAO usuarioDAO;
-	
+
 	@PostConstruct
 	public void listar() {
 		try {
-			
+
 			cargoDAO = new CargoDAO();
-			
-			
+
 			UsuarioDAO usuarioDAO = new UsuarioDAO();
-			
+
 			usuario = new Usuario();
 			cargos = cargoDAO.listar();
 			usuarios = usuarioDAO.listar();
-			
+
 		} catch (Exception erro) {
 			Messages.addFlashGlobalError("Ocorreu um erro ao tentar listar os usuarios");
 			erro.printStackTrace();
 		}
 	}
-	
+
+	public void novo() {
+		usuario = new Usuario();
+	}
+
 	public void salvar() {
 		try {
-		
-			UsuarioDAO usuarioDAO= new UsuarioDAO();
+
+			UsuarioDAO usuarioDAO = new UsuarioDAO();
 			usuarioDAO.merge(usuario);
-			
-			//usuario = new Usuario();
-			
+
+			usuario = new Usuario();
 			cargoDAO = new CargoDAO();
 			usuarios = usuarioDAO.listar();
 			cargos = cargoDAO.listar();
-			
+
 			Messages.addFlashGlobalInfo("usuário salvo com sucesso");
 		} catch (Exception erro) {
 			Messages.addFlashGlobalError("Ocorreu um erro ao tentar salvar os usuarios");
 			erro.printStackTrace();
-			
+
 		}
+	}
+
+	public void excluir(Usuario usuarioSeleciondo) {
+		try {
+			usuario = usuarioSeleciondo;
+			UsuarioDAO usuarioDAO = new UsuarioDAO();
+			usuarioDAO.excluir(usuario.getCodigo());
+			Messages.addGlobalInfo("Usuário removido com sucesso");
+
+			usuarios = usuarioDAO.listar();
+
+			// Messages.addGlobalInfo("Nome: " + usuario.getNome() + "cpf: " +
+			// usuario.getCpf() + "data: " + usuario.getDataNascimento() + "Sexo: " +
+			// usuario.getSexo() + "Cargo: " + usuario.getCargo());
+
+		} catch (Exception erro) {
+			Messages.addFlashGlobalError("Ocorreu um erro ao tentar remover os usuarios");
+			erro.printStackTrace();
+
+		}
+	}
+
+	public void editar(Usuario usuarioSeleciondo) {
+		usuario = usuarioSeleciondo;
 	}
 
 }
